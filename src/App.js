0f360bbc13bd450d4e1a642a62342ca70afdbc97
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-02-02 07:41:02 -0800

    Add components to application file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ListComponent from './ListComponent';
+import MapComponent from './MapComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
-    );
+      <div className="app">
+        <div className="list-container">
+          <h2>Neighborhood Map</h2>
+          <ListComponent/>
+        </div>
+        <div className="map-container">
+          <MapComponent/>
+        </div>
+      </div>
+    )
   }
 }
 
---
+++ src/ListComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ListComponent extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default ListComponent;
